9bdf349cf7c0fefc1efb9c14798e9a35eb92f2a9
Helton Higute <helton.higute@gmail.com>
2020-04-17 15:54:11 -0400

    Pageable

--- src/main/java/com/h3b/investment/controller/IofController.java
+++ src/main/java/com/h3b/investment/controller/IofController.java
@@ -1,12 +1,18 @@
 package com.h3b.investment.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.h3b.investment.exception.ResourceNotFoundException;
@@ -26,6 +32,23 @@ public class IofController{
 	@ApiOperation(value="List all IOF fees")
 	public List<Iof> listIofs(){
 		return iofRepository.findAll();
+	}
+	
+	@GetMapping("/iof/pageable")
+	@ApiOperation(value="List all IOF fees")
+	public List<Iof> listIofsPageable(
+            @RequestParam(defaultValue = "0") Integer pageNo, 
+            @RequestParam(defaultValue = "10") Integer pageSize,
+            @RequestParam(defaultValue = "nrDay") String sortBy){
+		
+		Pageable paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
+		Page<Iof> pagedResult = iofRepository.findAll(paging);
+		
+		if(pagedResult.hasContent()) {
+	        return pagedResult.getContent();
+	    } else {
+	    	return new ArrayList<Iof>();
+	    }
 	}
 
 	@GetMapping("/iof/{nrDay}")
